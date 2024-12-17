03da5c7e19e83b7fde6a671d5c6c022e6918bd7d
Ayushi Sahu <151500542+Ayushi2006Sahu@users.noreply.github.com>
2024-12-17 15:19:15 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/ayushi2006sahu",
   resume:
-    "https://drive.google.com/file/d/16LG4f_jbsA8FV5CNjaNsHBVdqMm_6KFY/view?usp=drivesdk ",
+    "https://drive.google.com/file/d/10iITUOmBaiPYUg_sxuKRS2NGLEfBDXgj/view?usp=sharing ",
   linkedin: "https://www.linkedin.com/in/ayushi-sahu-419921294/",
   
 };
